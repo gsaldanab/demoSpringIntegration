d990bae94a96a03f556c3586bb27d29ed4bfe4eb
George Saldana <gsaldanab@gmail.com>
2021-04-19 21:49:09 -0500

    modificacion de servicio
    diferentes codigos de estado dependiendo de la opcion indicada como payload

--- src/main/java/com/geo/demospringintegration/endpoint/WelcomeEndpoint.java
+++ src/main/java/com/geo/demospringintegration/endpoint/WelcomeEndpoint.java
@@ -7,30 +7,56 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.integration.support.MessageBuilder;
-import org.springframework.integration.support.MessageBuilderFactory;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
 
 import com.geo.demospringintegration.model.entity.HelloMsg;
+import com.geo.demospringintegration.model.entity.ResponseMsg;
 
 @Component
 public class WelcomeEndpoint {
 	private Logger log = LoggerFactory.getLogger(this.getClass().getName());
+	private static final int STATUS_OK = 0;
+	private static final int STATUS_ERROR = 1;
 
 	public Message<?> get(Message<String> msg) {
-		String name = msg.getPayload();
-		// log
-		log.info("Request with name: " + name);
+		String option = msg.getPayload();
+		log.info("Request with option: " + option);
+
+		return MessageBuilder.withPayload(getResponse(option)).copyHeadersIfAbsent(msg.getHeaders())
+				.setHeader("http_statusCode", HttpStatus.OK).build();
+	}
+
+	private ResponseMsg getResponse(String option) {
+		ResponseMsg response = new ResponseMsg();
+		HelloMsg returnMsg = new HelloMsg();
+		String messageOK = "Option %s executed succesfully";
+		String messageError = "Option %s failed";
+
+		switch (option) {
+		case "A":
+		case "B":
+		case "C":
+			returnMsg = new HelloMsg(String.format(messageOK, option), getCurrentTime());
+			response.setResponseStatus(STATUS_OK);
+			response.setResponseBody(returnMsg);
+			return response;
+		case "D":
+		case "E":
+			returnMsg = new HelloMsg(String.format(messageError, option), getCurrentTime());
+			response.setResponseStatus(STATUS_ERROR);
+			response.setResponseBody(returnMsg);
+			return response;
+		default:
+			response.setResponseStatus(STATUS_ERROR);
+			return response;
+		}
+	}
+
+	private String getCurrentTime() {
 		// get current time
 		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
 		LocalDateTime now = LocalDateTime.now();
-		String currentTime = dtf.format(now);
-
-		String strMsg = "Hello " + name + "! Welcome to spring integration with Spring boot!";
-
-		HelloMsg returnMsg = new HelloMsg(strMsg, currentTime);
-
-		return MessageBuilder.withPayload(returnMsg).copyHeadersIfAbsent(msg.getHeaders())
-				.setHeader("http_statusCode", HttpStatus.OK).build();
+		return dtf.format(now);
 	}
 }
